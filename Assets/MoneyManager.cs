25df33e7244f7739a6e4800f4f7b9b45fd7eb3d1
gonnosuke <gonzamurai@gmail.com>
2019-10-19 20:32:39 +0900

    シーンメニュー追加

---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+
+    public GameObject money_object = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text money_text = money_object.GetComponent<Text>();
+
+        money_text.text = "" + Player.money;
+    }
+}
