96f38a82f10bea1d042af32627c65883e8c6e514
gonnosuke <gonzamurai@gmail.com>
2019-10-19 16:10:48 +0900

    シーン遷移

---
+++ Assets/FishAndMoney.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAndMoney : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RetuenMoney(int id)
+    {
+        Player.money += id * 100 + 100;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static int money;
+    public static Object.Fish[] fish;
+    public static Object.Ship[] ships;
+    public static Object.Tool[] tools;
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = new Player();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
